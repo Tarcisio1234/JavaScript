788942389d4d2977b5d971b06390525cfc6bfc27
Tarcisio1234 <tarcisioalvesmachado2000@gmail.com>
2024-12-03 22:16:06 -0300

    add files

---
+++ Var e Let/index.js
@@ -0,0 +1,13 @@
+// diferença entre var e let
+var nome = "Tarcisio"
+var nome = "Tarcisio Alves"
+
+console.log("Olá meu nome é", nome)
+
+// Com o var é possivel redeclarar as variaveis
+
+let nome = "Tarcisio"
+let nome = "Tarcisio Alves"
+console.log("Olá meu nome é", nome)
+
+//Com o let não é possivel redeclarar as variaveis
