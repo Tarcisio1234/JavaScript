e4b73e836515a28bc6294e1acd21d97c50c0593e
Tarcisio1234 <tarcisioalvesmachado2000@gmail.com>
2024-12-04 21:58:24 -0300

    add files

---
+++ Alert, confirm e pronpt/js/script.js
@@ -0,0 +1,7 @@
+let numero1 = prompt('Digite um número: ')
+let numero2 = prompt('Digite um 2° número: ')
+numero1 = Number(numero1)
+numero2 = Number(numero2)
+
+let resultado = numero1 + numero2
+console.log('O valor da conta é: ', resultado)
